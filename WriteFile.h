48603927719da4550994d5037579940d63971788
Andrew Hall <drewbear.hall2@gmail.com>
2014-09-06 15:26:26 -0700

    Here are the Write Files

---
+++ WriteFile.h
@@ -0,0 +1,25 @@
+#if !defined WRITE_FILE
+#define WRITE_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class WriteFile
+{
+	private:
+   ofstream output_file;
+   bool closed;
+   
+   public:
+    WriteFile(const char* file_name);
+
+void writeLine(String* line);
+void close();
+	~WriteFile();
+};
+
+
+
+#endif
